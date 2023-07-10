6de4cac104f4b21e758c434b67c4ebe388bdcd61
Rendell Arruda <rendellarruda3@gmail.com>
2023-07-10 10:46:00 -0300

    envio de arq

--- src/currentConverter/CaixaDeDialogo.java
+++ src/currentConverter/CaixaDeDialogo.java
@@ -7,13 +7,14 @@ public class CaixaDeDialogo {
 	public static void main(String[] args) {
 //		String nome = null;
 //		int resposta;
-		
-		String[] options = {"Conversor de Moedas", "Conversor de temperaturas", "Conversor de medidas", "Conversor de Distâncias"};
 
-		int selectedValue = JOptionPane.showOptionDialog(null, "Escolha uma opção ", "Menu",JOptionPane.INFORMATION_MESSAGE,0, null, options, options[0]);
+		String[] options = { "Conversor de Moedas", "Conversor de temperaturas", "Conversor de medidas",
+				"Conversor de Distâncias" };
+
+		int selectedValue = JOptionPane.showOptionDialog(null, "Escolha uma opção ", "Menu",
+				JOptionPane.INFORMATION_MESSAGE, 0, null, options, options[0]);
 		System.out.println(selectedValue);
-		
-		
+
 //		resposta = JOptionPane.showConfirmDialog(null, "O seu nome é " + nome + "?");
 
 //		if (resposta == JOptionPane.YES_OPTION) {
